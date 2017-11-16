85f97686e28a0663fd65e9ba1ac92ffbae0aca87
ALUMNADO\sacoza <esendier.23@gmail.com>
2017-11-16 10:08:27 +0100

    ahor3 modificado

--- ahor3/ahor3.js
+++ ahor3/ahor3.js
@@ -1,8 +1,8 @@
 var solucion=[];
-var condicion=true;
+var condicion=false;
 var intentos=0;
 var aciertos=0;
-var palabra=["hola","hora","perro","algo","sapo"];
+var palabra=["hola","aburro","perro","agarro","ahorro"];
 var aleat=Math.floor(Math.random()*palabra.length);
 var adivinar=palabra[aleat];
 
@@ -12,28 +12,26 @@ console.log(aleat);
 
 
 for (var i = 0; i < adivinar.length; i++) {
-  solucion[i]='o';
+  solucion[i]='0';
 
   console.log(i);
 }
 
-  while (intentos<=5 && aciertos<adivinar.length) {
+  while (intentos<5 && aciertos<adivinar.length) {
         var letra=prompt("Inroduce una letra: ");
         var acertado=false;
 
+letra=prompt("Inroduce otra letra: ");
 for (var i = 0; i < adivinar.length; i++) {
-if (letra==adivinar.charAt(i)) {
+if (adivinar.charAt(i)==letra) {
 solucion[i]=adivinar.charAt(i);
 acertado=true;
-i++;
-
+aciertos++;
 console.log(aciertos);
     }
+    else{
+    }
   }
-aciertos++;
-}
-
-
 
 if (acertado==true) {
   alert("Correcto")
@@ -41,11 +39,9 @@ if (acertado==true) {
   alert("Fallo")
   intentos++;
 }
-
-if (acertado==true) {
-  document.write("_");
-}else {
-  document.write(solucion);
 }
+
+  document.write("La palabra es ",adivinar);
+
 
 console.log(solucion);
