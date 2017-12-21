ad6f6b7ea750eeda6b5fd97d6464e6427f5603f4
ALUMNADO\sacoza <esendier.23@gmail.com>
2017-12-21 08:39:55 +0100

    Intro PHP

---
+++ IntroPHP/home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color:blue;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #111;
+}
+</style>
+</head>
+<body>
+<?php $were="home" ?>
+<ul>
+  <li><a class= "<?php if ($were=="home") {
+    echo "active";
+  } ?>" href="home.php">Home</a></li>
+  <li><a class= "<?php if ($were=="News") {
+    echo "active";
+  } ?>" href="news.php">News</a></li>
+  <li><a class= "<?php if ($were=="Contact") {
+    echo "active";
+  } ?>" href="#contact">Contact</a></li>
+</ul>
+
+</body>
+</html>
