a5a4ec3a8ea2c06a8049eb0a1d926f77150d1291
ALUMNADO\sacoza <esendier.23@gmail.com>
2018-01-30 12:45:42 +0100

    proyecto 5

---
+++ PHP/proy5/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<form class="" action="insertar.php" method="post">
+
+  <input type="text" name="nombre" value="nombre" onFocus="this.value=''">
+  <input type="text" name="apellidos" value="Apellidos" onFocus="this.value=''">
+  <input type="text" name="edad" value="Edad" onFocus="this.value=''">
+  <input type="text" name="curso" value="Curso" onFocus="this.value=''">
+  <input type="text" name="puntuacion" value="Puntuacion" onFocus="this.value=''">
+  <br>
+  <input type="submit" name="Enviar" value="Enviar">
+</form>
+
+  </body>
+</html>
---
+++ PHP/proy5/insertar.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <a href="index.php">INDEX</a>
+<?php
+
+$conector = new mysqli("localhost", "root", "", "juegos");
+if ($conector-> connect_errno) {
+    echo "Fallo al conectar a MySQL: " . $mysqli->connect_errno;
+
+}else{
+$nombre=$_POST['nombre'];
+$apellidos=$_POST['apellidos'];
+$edad=$_POST['edad'];
+$curso=$_POST['curso'];
+$puntuacion=$_POST['puntuacion'];
+
+$consulta="INSERT INTO usuarios (nombre,apellidos,edad,curso,puntuacion) values ('$nombre','$apellidos',$edad,$curso,$puntuacion)";
+
+$insert=$conector->query($consulta);
+$resultado=$conector->query($consulta="SELECT * FROM usuarios");
+foreach($resultado as $fila){
+
+echo "<br>ID: ".$fila['id'];
+echo "<br>Nombre: ".$fila['nombre'];
+echo "<br>Apellidos: ".$fila['apellidos'];
+echo "<br>Edad: ".$fila['edad'];
+echo "<br>Curso: ".$fila['curso'];
+echo "<br>PUTA: ".$fila['puntuacion'];
+echo "<br>";
+   }
+ }
+
+?>
+
+  </body>
+</html>
